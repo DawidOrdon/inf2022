843f53f5f27e1d9b640baa0abac7a33f17c3bcc3
DawidOrdon <dawid.ordon2000@gmail.com>
2025-09-05 11:18:05 +0200

    .

---
+++ 05.09.2025/index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //if
+        $zmienna=43;
+        if($zmienna>=10){
+            echo $zmienna.">10<br />";
+        }
+        if ($zmienna>=20){
+            echo $zmienna.">20<br />";
+        }
+        if($zmienna>=30) {
+            echo $zmienna . ">30<br />";
+        }
+        if($zmienna>=40){
+            echo $zmienna. ">40<br />";
+        }
+
+        echo "<br /><br /><br /><br /><br />";
+
+    //przelacz w odniesieniu do parametru w nawiasach
+        $cos=9;
+        switch ($cos) {
+            case 1:{
+                echo "1<br />";
+                break;
+            }
+            case 2:{
+                echo "2<br />";
+                break;
+            }
+            case 3:{
+                echo "3<br />";
+                break;
+            }
+            case 4:{
+                echo "4<br />";
+                break;
+            }
+            default:{
+                echo"inne";
+                break;
+            }
+        }
+    ?>
+<!--    do przesylani danych dla php-->
+    <form action="" method="post">
+        <input type="number" name="name" id="">
+        <input type="checkbox" name="regulamin" id="">
+        <input type="submit" value="post">
+    </form>
+    <?php
+//        jak w formie mamy method post to $_POST a jak get $_GET a w kwadratowych nawiasach name z inputa
+    if(!empty($_POST["name"])){
+        if(is_numeric($_POST["name"])&&$_POST["name"]>10){
+            echo "liczba wieksza od 10";
+        }else{
+            echo "cos innego";
+        }
+
+    }else{
+        echo "nic nie ma";
+    }
+
+    ?>
+</body>
+</html>
