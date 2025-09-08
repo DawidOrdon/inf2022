ef14794dc0fe30968dbb99a87b293acb898fa142
DawidOrdon <dawid.ordon2000@gmail.com>
2025-09-08 17:06:23 +0200

    .

--- 08.09.2025/index.php
+++ 08.09.2025/index.php
@@ -10,6 +10,7 @@
 <body>
 <form action="" method="post">
     <label for="imie">imie</label><input type="text" name="imie" id="imie"><br />
+    <label for="wiek">wiek</label><input type="number" name="wiek" id="wiek"><br />
     <label for="regulamin">regulamin</label><input type="checkbox" name="regulamin" id="regulamin"><br />
     <input type="submit" value="przeslij">
 </form>
@@ -20,6 +21,12 @@ $error=0;
     }else{
         $error=1;
     }
+    if(!empty($_POST['wiek'])&&is_numeric($_POST['wiek'])&&$_POST['wiek']>0){
+        $wiek=$_POST['wiek'];
+    }else{
+        $error=1;
+    }
+
     if(isset($_POST['regulamin'])){
         $regulamin=true;
     }else{
@@ -28,12 +35,15 @@ $error=0;
 
     if($error==0){
         echo "imie: $imie<br />";
+        echo "wiek: $wiek<br />";
         echo "czy zaakceptował regulamin:";
         if($regulamin){
             echo "tak";
         }else{
             echo "nie";
         }
+    }else{
+        echo"podaj prawidłowe dane";
     }
 ?>
 </body>
