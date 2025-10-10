bce45637f94857ba8e15f5c5054a02be2e74d595
DawidOrdon <dawid.ordon2000@gmail.com>
2025-10-10 09:34:56 +0200

    .

--- szbd/index.php
+++ szbd/index.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
-    if(!empty($_POST['baza'])){
-        $_SESSION['baza'] = $_POST['baza'];
-    }
+if(!isset($_SESSION['baza'])){
+    $_SESSION['baza'] = "mysql";
+}
+if(!empty($_POST['baza'])){
+    $_SESSION['baza'] = $_POST['baza'];
+}
+if(!empty($_POST['zapytanie'])){
+    $_SESSION['zapytanie'] = $_POST['zapytanie'];
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -13,8 +19,11 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-        .bazy, .tabele{
+        .bazy, .tabele, .zapytanie{
             float: left;
+        }
+        .zapytanie{
+            width: 50%;
         }
     </style>
 </head>
@@ -29,7 +38,7 @@ session_start();
             </tr>
 
         <?php
-            $db = new mysqli("localhost", "root", "");
+            $db = new mysqli("localhost", "root", "",$_SESSION['baza']);
             $sql='show databases';
             $query = $db->prepare($sql);
             $query->execute();
@@ -42,11 +51,53 @@ session_start();
     </form>
 </div>
 <div class="tabele">
+    <table>
     <?php
         if(isset($_SESSION['baza'])){
-            echo"wybrales/as baze: ".$_SESSION['baza'];
+            echo"wybrales/as baze: ".$_SESSION['baza']."<br />";
+            $sql='show tables';
+            $query = $db->prepare($sql);
+            $query->execute();
+            $result=$query->get_result();
+            if($result->num_rows==0){
+                echo"<tr><td>W danej bazie nie ma tabel</td></tr>";
+            }
+            while ($row=$result->fetch_array()){
+                echo"<tr><td>$row[0]</td></tr>";
+            }
         }
     ?>
+    </table>
+</div>
+<div class="zapytanie">
+    <form action="" method="post">
+        <textarea name="zapytanie" id="" cols="30" rows="5"><?php echo $_SESSION['zapytanie']!=null ? $_SESSION['zapytanie'] : "";?></textarea><br />
+        <button>wyslij</button>
+    </form>
+    <table>
+        <pre>
+        <?php
+        if(isset($_SESSION['zapytanie'])){
+            $query=$db->prepare($_SESSION['zapytanie']);
+            $query->execute();
+            $result=$query->get_result();
+            if($result->num_rows==0){
+
+            }else{
+                $row=$result->fetch_assoc();
+                foreach($row as $key=>$value){
+                    echo "key: ".$key." value: $value<br />";
+                }
+
+                while($row=$result->fetch_assoc()){
+                    print_r($row);
+                }
+            }
+        }
+
+        ?>
+            </pre>
+    </table>
 </div>
 </body>
 </html>
